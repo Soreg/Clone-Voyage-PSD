524b431a60613c1cfc12311913d1048cd61beba4
unknown <S_Gravesen92@hotmail.com>
2018-02-18 22:42:22 +0100

    Finished specials section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 StartMainSlider();
+
+// Calculate if image if tall or wide
+$(document).ready(function(){
+    $('.packages').find('img').each(function(){
+     var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+     $(this).addClass(imgClass);
+    })
+})
