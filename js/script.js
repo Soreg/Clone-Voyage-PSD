2c94e4b26332a41f2396c938f1ca2a1fd883d6fe
unknown <S_Gravesen92@hotmail.com>
2018-02-19 21:26:23 +0100

    Finished holidays section with added jquery and images

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@ StartMainSlider();
 
 // Calculate if image if tall or wide
 $(document).ready(function(){
-    $('.packages').find('img').each(function(){
+    $('#specials .packages').find('img').each(function(){
      var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
      $(this).addClass(imgClass);
     })
-})
+});
+
+// When clicking on holidays package (change class)
+$("#holiday-type .package").click(function() {
+    var imgToDisplay = $(this).attr("data-image");
+    
+    $("#holiday-type .package").removeClass("selected");
+    $(this).addClass("selected");
+
+    $("#holiday-type").css("background-image", "url(" + imgToDisplay + ")");
+});
