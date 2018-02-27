8cbe2e47921692b70c1e400ad9d7641b73fd1914
unknown <S_Gravesen92@hotmail.com>
2018-02-27 23:41:20 +0100

    Add: Added very basic image preloader

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-StartMainSlider();
-StartReviewSlider();
-
 // Calculate if image if tall or wide
 $(document).ready(function(){
+
+    // Preload all images before loading
+    $("body").hide();
+    var total_images = $("body img").length;
+    var images_loaded = 0;
+    $("body").find('img').each(function() {
+        var fakeSrc = $(this).attr('src');
+        $("<img/>").attr("src", fakeSrc).css('display', 'none').on('load', function() {
+            images_loaded++;
+            if (images_loaded >= total_images) {
+                // now all images are loaded.
+                $("body").show();
+            }
+        });
+    });
+
+    // Assign classes to images
     $('#specials .packages').find('img').each(function(){
      var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
      $(this).addClass(imgClass);
@@ -18,5 +32,9 @@ $("#holiday-type .package").click(function() {
     
     $("#holiday-type").css("background-image", "url(" + imgToDisplay + ")");
 });
+
+// start sliders
+StartMainSlider();
+StartReviewSlider();
 
 });
