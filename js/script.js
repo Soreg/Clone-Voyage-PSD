6eb79a6f26ce1c270a529cf1c37276942a60d8ef
unknown <S_Gravesen92@hotmail.com>
2018-02-28 14:32:42 +0100

    Added percentage to preloader

--- js/script.js
+++ js/script.js
@@ -2,23 +2,35 @@
 $(document).ready(function(){
 
     // Preload all images before loading
-    var fadeTime = 1000;
+var preloadImages = function() {
     $("body").css("overflow-y", "hidden");
+    var fadeTime = 1000;
+    var percentComplete = 0;
     var total_images = $("body img").length;
     var images_loaded = 0;
+    var percentPerImage = 100 / total_images;
+    
     $("body").find('img').each(function() {
         var fakeSrc = $(this).attr('src');
         $("<img/>").attr("src", fakeSrc).css('display', 'none').on('load', function() {
             images_loaded++;
+            percentComplete += percentPerImage;
+            $(".preloader .percentage").html(Math.round(percentComplete) + " % complete ...");
+            
             if (images_loaded >= total_images) {
                 // When images are loaded
+                $(".preloader h2:first-child").html("Preload complete");
+                $(".preloader .percentage").html("100%");
+                
                 $( ".preloader" ).fadeOut(fadeTime, function() {
+                    // After fade is done
                     $("body").css("overflow-y", "scroll");
                     $(".preloader").css("display", "none");
                 });
             }
         });
     });
+}
 
     // Assign classes to images
     $('#specials .packages').find('img').each(function(){
@@ -36,6 +48,7 @@ $(document).ready(function(){
     });
 
 // start sliders
+preloadImages();
 StartMainSlider();
 StartReviewSlider();
 
