7c28ea9a5490e51f85db385d476f569b6295400d
unknown <S_Gravesen92@hotmail.com>
2018-03-02 00:09:47 +0100

    Optimized slider - buttons now working, and slider is looping

--- js/main-slider.js
+++ js/main-slider.js
@@ -8,47 +8,69 @@ var StartMainSlider = function() {
     var screenWidth = $("#slider-section .slide").width();
 
     var amountOfClasses = $("#slider-section .slide").length;
-    var currentSlide = 0;
     
-    var animateDirection = "-=";
+
+    // When slider slides
+    // Copy first slide to the end of the file
+    // Remove first slide
+
+    // Logic used when slide is done (append and remove slides)
+    function slideLogic(direction) {
+
+        var firstSlide = $("#slider-section .slide").first();
+        var selectedSlide = $("#slider-section .slides").children().eq(1);
+        var clone = selectedSlide.clone(); // Clone since replaceWith removes selected element (the clone)
+
+        firstSlide.clone().appendTo(slides); // Copy and paste first slide to end of row
+
+        firstSlide.replaceWith(clone); // Set first slide to current slide
+        slides.css("margin-left", "0"); // Move slider to first slide
+
+        selectedSlide.remove(); // Remove previous slide
+    }
+
+    function addPreviousSlider() {
+        var lastSlide = $("#slider-section .slide").last();
+        var clone = lastSlide.clone();
+        clone.prependTo(slides);
+        slides.css("margin-left", "-=" + screenWidth);
+        lastSlide.remove();
+    }
     
     // Slide
     var slideTimer = setInterval(function() {
-        slides.animate({"margin-left": animateDirection + screenWidth}, slideSpeed);
-        if(animateDirection == "-=") {
-            currentSlide++;
-        }
-        else {
-            currentSlide--;
-        }
-
-        if(currentSlide == amountOfClasses-1) {
-            animateDirection = "+=";
-        }
-        else if(currentSlide == 0) {
-            animateDirection = "-=";
-        }
+        slides.animate({"margin-left": "-=" + screenWidth}, slideSpeed, function() {
+            slideLogic("right");
+        });
     }, timeBetweenSlides);
 
     // Pause on hover
-    $("#slider-section .slides").mouseover(function() {
+    $("#slider-section .slides, #slider-section .slider-arrow").mouseover(function() {
         clearInterval(slideTimer);
-    }).mouseout(function() {
+    })
+    .mouseout(function() {
         slideTimer = setInterval(function() {
-            slides.animate({"margin-left": animateDirection + screenWidth}, slideSpeed);
-            if(animateDirection == "-=") {
-                currentSlide++;
-            }
-            else {
-                currentSlide--;
-            }
-    
-            if(currentSlide == amountOfClasses-1) {
-                animateDirection = "+=";
-            }
-            else if(currentSlide == 0) {
-                animateDirection = "-=";
-            }
+            slides.animate({"margin-left": "-=" + screenWidth}, slideSpeed, function() {
+                slideLogic("right");
+            });
         }, timeBetweenSlides);
     });
+
+    // when clicking the next-button
+    $(".next-arrow").click(function() {
+        slides.finish();
+        slides.animate({"margin-left": "-=" + screenWidth}, slideSpeed, function() {
+            slideLogic("right");
+        });
+    });
+
+    // when clicking the prev-button
+    $(".prev-arrow").click(function() {
+        slides.finish();
+        addPreviousSlider();
+        slides.animate({"margin-left": "+=" + screenWidth}, slideSpeed, function() {
+            // slideLogic("right");
+        });
+    });
+
 }
