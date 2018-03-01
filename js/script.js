29ac4550ac3bc3f8a4b500b5731d2dc109966216
unknown <S_Gravesen92@hotmail.com>
2018-03-01 12:20:55 +0100

    Finished bottom footer

--- js/script.js
+++ js/script.js
@@ -2,35 +2,35 @@
 $(document).ready(function(){
 
     // Preload all images before loading
-var preloadImages = function() {
-    $("body").css("overflow-y", "hidden");
-    var fadeTime = 1000;
-    var percentComplete = 0;
-    var total_images = $("body img").length;
-    var images_loaded = 0;
-    var percentPerImage = 100 / total_images;
-    
-    $("body").find('img').each(function() {
-        var fakeSrc = $(this).attr('src');
-        $("<img/>").attr("src", fakeSrc).css('display', 'none').on('load', function() {
-            images_loaded++;
-            percentComplete += percentPerImage;
-            $(".preloader .percentage").html(Math.round(percentComplete) + " % complete ...");
-            
-            if (images_loaded >= total_images) {
-                // When images are loaded
-                $(".preloader h2:first-child").html("Preload complete");
-                $(".preloader .percentage").html("100%");
+    var preloadImages = function() {
+        $("body").css("overflow-y", "hidden");
+        var fadeTime = 1000;
+        var percentComplete = 0;
+        var total_images = $("body img").length;
+        var images_loaded = 0;
+        var percentPerImage = 100 / total_images;
+        
+        $("body").find('img').each(function() {
+            var fakeSrc = $(this).attr('src');
+            $("<img/>").attr("src", fakeSrc).css('display', 'none').on('load', function() {
+                images_loaded++;
+                percentComplete += percentPerImage;
+                $(".preloader .percentage").html(Math.round(percentComplete) + " % complete ...");
                 
-                $( ".preloader" ).fadeOut(fadeTime, function() {
-                    // After fade is done
-                    $("body").css("overflow-y", "scroll");
-                    $(".preloader").css("display", "none");
-                });
-            }
+                if (images_loaded >= total_images) {
+                    // When images are loaded
+                    $(".preloader h2:first-child").html("Preload complete");
+                    $(".preloader .percentage").html("100%");
+                    
+                    $( ".preloader" ).fadeOut(fadeTime, function() {
+                        // After fade is done
+                        $("body").css("overflow-y", "scroll");
+                        $(".preloader").css("display", "none");
+                    });
+                }
+            });
         });
-    });
-}
+    }
 
     // Assign classes to images
     $('#specials .packages').find('img').each(function(){
@@ -45,6 +45,13 @@ var preloadImages = function() {
 
         $("#holiday-type .package").removeClass("selected");
         package.addClass("selected");
+    });
+
+    // Scroll to top when clicking on bottom arrow
+    $(".arrow-up").click(function() {
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 1000);
     });
 
 // start sliders
