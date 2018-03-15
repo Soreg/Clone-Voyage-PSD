21863789f4a0439ef452fd58a8793e259b62c5e0
unknown <S_Gravesen92@hotmail.com>
2018-03-15 10:58:53 +0100

    Switchec out image preloader with lazyload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,26 @@
-// Calculate if image if tall or wide
 $(document).ready(function(){
 
-    // Preload all images before loading
-    var preloadImages = function() {
-        $("body").css("overflow-y", "hidden");
-        var fadeTime = 1000;
-        var percentComplete = 0;
-        var total_images = $("body img").length;
-        var images_loaded = 0;
-        var percentPerImage = 100 / total_images;
-        
-        $("body").find('img').each(function() {
-            var fakeSrc = $(this).attr('src');
-            $("<img/>").attr("src", fakeSrc).css('display', 'none').on('load', function() {
-                images_loaded++;
-                percentComplete += percentPerImage;
-                $(".preloader .percentage").html(Math.round(percentComplete) + " % complete ...");
+    var totalSlidesToLoad = ($(".main-slider-img").length);
+    
+    var totalSlidesLoaded = 0;
+    
+
+    $(function() {
+        $('.lazy').lazy({
+            afterLoad: function(element) {
                 
-                if (images_loaded >= total_images) {
-                    // When images are loaded
-                    $(".preloader h2:first-child").html("Preload complete");
-                    $(".preloader .percentage").html("100%");
-                    
-                    $( ".preloader" ).fadeOut(fadeTime, function() {
-                        // After fade is done
-                        $("body").css("overflow-y", "scroll");
-                        $(".preloader").css("display", "none");
-                    });
+                if(element.hasClass("main-slider-img")) {
+                    totalSlidesLoaded++;
+
+                    // check if all is loaded
+                    if(totalSlidesLoaded == totalSlidesToLoad) {
+                        StartMainSlider();
+                    }
                 }
-            });
+            }
         });
-    }
+    });
+
 
     // Assign classes to images
     $('#specials .packages').find('img').each(function(){
@@ -54,10 +44,8 @@ $(document).ready(function(){
         }, 1000);
     });
 
-// start sliders
-preloadImages();
-StartMainSlider();
-StartReviewSlider();
+    StartReviewSlider();
+    
 
 
 // Burger menu on-click
